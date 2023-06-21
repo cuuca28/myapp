b20473a52db1595f77fa6a4cfb63a05fdeb6df5e
Romina Betancor <romibetan7@gmail.com>
2023-06-20 22:01:02 -0300

    segundapreentrega

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,76 @@
+import { useEffect } from 'react'
+import '../App.css'
+
+const MOCK_DATA = [
+    {
+        id: 1,
+        nombre: "Blunt",
+        descripción: "El mejor sabor para quemar",
+        precio: 150,
+        img: "../img/"
+    },
+    {
+        id: 2,
+        nombre: "Tierra",
+        descripción: "La tierra con el abono de ph justo",
+        precio: 450,
+        img: "../img/"
+    },
+    {
+        id: 3,
+        nombre: "Hojillas",
+        descripción: "Hojillas con pegamiento de miel",
+        precio: 50,
+        img: "../img/"
+    },
+    {
+        id: 4,
+        nombre: "Bóveda",
+        descripción: "Sobre bóveda ajusta la humedad perfecta",
+        precio: 250,
+        img: "../img/"
+    }
+]
+
+const pedirDatos = () => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve (MOCK_DATA)         
+        }, 5000)
+     })
+} 
+
+const ItemListContainer = () => {
+
+    const [productos, setProcutos] = useState ([])
+    console.log(productos)  
 
 
-const ItemListContainer = ( {mensaje} ) => {
+    useEffect (() => {
+        pedirDatos()
+            .then((res) => {
+                setProductos(res)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
 
     return(
         <div className="list_container">
             <h2>Los mejores productos para una excelente experiencia</h2>
             <hr />
 
-            <p>{mensaje}</p>
+            {
+                productos.map((prod) => (
+                    <div>
+                        <h4>{prod.nombre}</h4>
+                        <img src={prod.img} alt={prod.nombre} />
+                        <p>{prod.descripcion}</p>
+                        <p>Precio: ${prod.precio}</p>
+                ))
+            {'}'}
+
         </div>
     )
         
